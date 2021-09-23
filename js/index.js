c5051ce2f674c0464d9225858cad01a5a2218de4
Casey Rock <rockc@udel.edu>
2021-09-23 10:24:08 -0400

    added the script tag for index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
-$( document ).ready(function() {
-    $( "#submit-btn" ).on( "click", function( event ) {
+$(document).ready(function () {
+    $( "#submit-btn" ).on('click', function (e) {
+        e.preventDefault()
         let username = $("#username-input").val()
         let password = $("#password-input").val()
        
         console.log("Username: "+ username)
         console.log("Password: "+ password)
+        window.location.href = '../pages/beatmaker.html'
         return false
     });  
 });
