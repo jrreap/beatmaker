5564f44de1c9affe59e1e969828855538032aa00
Psy1ALise <54691057+Psy1ALise@users.noreply.github.com>
2021-12-04 23:03:42 -0500

    double click to delete notes/delete unneeded columns

--- js/beatmaker.js
+++ js/beatmaker.js
@@ -1,10 +1,10 @@
 const INSTRUMENTS = {
-  SYNTH: 'synth',
-  GUITAR: 'guitar',
-  PIANO: 'piano',
-  HORN: 'horn',
-  DRUM: 'drum',
-  BASS: 'bass'
+    SYNTH: 'synth',
+    GUITAR: 'guitar',
+    PIANO: 'piano',
+    HORN: 'horn',
+    DRUM: 'drum',
+    BASS: 'bass'
 }
 
 let sampleIndex = 1
@@ -18,99 +18,99 @@ $(document).ready(initialize)
  * Called once on page load. This is where all of the initialization logic goes
  */
 function initialize() {
-  const sessionId = sessionStorage.getItem('uid')
-  $.ajax({
-    url: '/authenticateRoute',
-    type: 'POST',
-    data: { uid: sessionId },
-    statusCode: {
-      200: function (result) {
-        if (result) {
-          generateWorkspace()
-          bindToInstrumentButtons()
-          bindToControlButtons()
-          logoutBtn()
+    const sessionId = sessionStorage.getItem('uid')
+    $.ajax({
+        url: '/authenticateRoute',
+        type: 'POST',
+        data: { uid: sessionId },
+        statusCode: {
+            200: function(result) {
+                if (result) {
+                    generateWorkspace()
+                    bindToInstrumentButtons()
+                    bindToControlButtons()
+                    logoutBtn()
+                }
+            },
+            203: function(result) {
+                window.location.href = '/index.html'
+            }
         }
-      },
-      203: function (result) {
-        window.location.href = '/index.html'
-      }
-    }
-  })
+    })
 }
 
 
 function logoutBtn() {
-  $('#logout-btn').on('click', function (e) {
-    $.ajax({
-      url: '/signOut',
-      type: 'POST',
-      statusCode: {
-        200: function (userID) {
-          sessionStorage.removeItem('uid')
-          window.location.href = '/beatmaker.html'
-        },
-        500: function (result) {
-          console.log(result)
-          // display_alert(result.replace("Firebase: ", ''), 'danger')
-        }
-      }
+    $('#logout-btn').on('click', function(e) {
+        $.ajax({
+            url: '/signOut',
+            type: 'POST',
+            statusCode: {
+                200: function(userID) {
+                    sessionStorage.removeItem('uid')
+                    window.location.href = '/beatmaker.html'
+                },
+                500: function(result) {
+                    console.log(result)
+                        // display_alert(result.replace("Firebase: ", ''), 'danger')
+                }
+            }
+        })
     })
-  })
 }
 
 /**
  * Generates the track rows and columns dynamically instead of duplicating the HTML statically
  */
 function generateWorkspace() {
-  const workspace = $('#workspace')
-  const icons = ['fa-wave-square', 'fa-guitar', 'fa-ruler-horizontal', 'fa-plus', 'fa-drum', 'fa-guitar']
-  const instrumentsByIndex = Object.values(INSTRUMENTS)
+    const workspace = $('#workspace')
+    const icons = ['fa-wave-square', 'fa-guitar', 'fa-ruler-horizontal', 'fa-plus', 'fa-drum', 'fa-guitar']
+    const instrumentsByIndex = Object.values(INSTRUMENTS)
 
-  // On initial load generate a nice amount of columns for the screen size
-  const colLimit = beatLength + 1
+    // On initial load generate a nice amount of columns for the screen size
+    const colLimit = beatLength + 1
 
-  for (let i = 0; i < 6; i++) {
-    const row = $('<div class="row track"></div>')
-    workspace.append(row)
-    beatMatrix[i] = []
+    for (let i = 0; i < 6; i++) {
+        const row = $('<div class="row track"></div>')
+        workspace.append(row)
+        beatMatrix[i] = []
 
-    // Add the column marker
-    const marker = $(`<div class="col track marker d-flex justify-content-center align-items-center"><i class="fas ${icons[i]} fa-2x"></i></div>`)
-    row.append(marker)
+        // Add the column marker
+        const marker = $(`<div class="col track marker d-flex justify-content-center align-items-center"><i class="fas ${icons[i]} fa-2x"></i></div>`)
+        row.append(marker)
 
-    for (let j = 0; j < colLimit; j++) {
-      const col = $(`<div id='track${i}-cell${j}' class="col track selector d-flex justify-content-center align-items-center"></div>`)
-      row.append(col)
+        for (let j = 0; j < colLimit; j++) {
+            const col = $(`<div id='track${i}-cell${j}' class="col track selector d-flex justify-content-center align-items-center"></div>`)
+            row.append(col)
 
-      beatMatrix[i][j] = ''
+            beatMatrix[i][j] = ''
 
-      col.on('click', () => { setSpaceInstrument(i, j, col, instrumentsByIndex[i]) })
+            col.on('click', () => { setSpaceInstrument(i, j, col, instrumentsByIndex[i]) })
+        }
     }
-  }
 
-  console.log(beatMatrix)
+    console.log(beatMatrix)
 }
 
 /**
  * Adds handlers to each of the instrument selection buttons dynamically
  */
 function bindToInstrumentButtons() {
-  $('.instrument').on('click', function (e) {
-    changeInstrument(e.currentTarget.id)
-  })
+    $('.instrument').on('click', function(e) {
+        changeInstrument(e.currentTarget.id)
+    })
 }
 
 /**
  * Adds handlers to each of the sample change buttons (next and previous)
  */
 function bindToControlButtons() {
-  $('#sample-prev').on('click', () => changeSample(-1))
-  $('#sample-next').on('click', () => changeSample(1))
-  $('#play').on('click', playBeat)
-  $('#eraser').on('click', () => {
-    erasing = !erasing
-  })
+    $('#sample-prev').on('click', () => changeSample(-1))
+    $('#sample-next').on('click', () => changeSample(1))
+    $('#play').on('click', playBeat)
+    $('#eraser').on('click', () => {
+        erasing = !erasing
+    })
 }
 
 /**
@@ -118,21 +118,21 @@ function bindToControlButtons() {
  * @param {number} change The number of samples to "loop" through. Can be negative to go reverse.
  */
 function changeSample(change) {
-  if (sampleIndex + change >= 1 && sampleIndex + change <= 6) {
-    sampleIndex += change
+    if (sampleIndex + change >= 1 && sampleIndex + change <= 6) {
+        sampleIndex += change
 
-    updateSampleDisplay()
-  }
+        updateSampleDisplay()
+    }
 }
 
 /**
  * Reads in the beat matrix and plays back the audio
  */
 async function playBeat() {
-  const mappedMatrix = Object.values(beatMatrix)
+    const mappedMatrix = Object.values(beatMatrix)
 
-  const soundBoard = new SoundBoard(mappedMatrix)
-  soundBoard.play(500)
+    const soundBoard = new SoundBoard(mappedMatrix)
+    soundBoard.play(500)
 }
 
 /* LISTENERS and UTILITIES */
@@ -141,11 +141,11 @@ async function playBeat() {
  * @param {number} instrument The instrument code to select
  */
 function changeInstrument(instrument) {
-  console.log('Instrument changed to ' + instrument)
-  currentInstrument = instrument
-  sampleIndex = 1
+    console.log('Instrument changed to ' + instrument)
+    currentInstrument = instrument
+    sampleIndex = 1
 
-  updateSampleDisplay()
+    updateSampleDisplay()
 }
 
 /**
@@ -154,26 +154,27 @@ function changeInstrument(instrument) {
  * @param {number} instrument The instrument code
  */
 function setSpaceInstrument(row, col, element, instrument) {
-  console.log('Set instrument space to ' + instrument)
-  element.text('')
+    console.log('Set instrument space to ' + instrument)
+    console.log(element)
+    element.text('')
 
-  if (!erasing) {
-    element.append(instrument)
-    beatMatrix[row][col] = instrument + sampleIndex
-  } else {
-    beatMatrix[row][col] = '' // Eraser mode
-  }
+    if (beatMatrix[row][col] == '') {
+        element.append(instrument)
+        beatMatrix[row][col] = instrument + sampleIndex
+    } else {
+        beatMatrix[row][col] = '' // Eraser mode
+    }
 
-  console.log(beatMatrix)
+    console.log(beatMatrix)
 }
 
 /**
  * Updates the sample display to match the current selected sampleIndex
  */
 function updateSampleDisplay() {
-  const sample = $('#sample-index')
-  sample.text('')
-  sample.append(sampleIndex)
+    const sample = $('#sample-index')
+    sample.text('')
+    sample.append(sampleIndex)
 }
 
 /**
@@ -182,51 +183,51 @@ function updateSampleDisplay() {
  * @returns {Promise}
  */
 function sleep(delay) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delay)
-  })
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay)
+    })
 }
 
 function SoundBoard(mappedMatrix) {
-  const instrumentsByIndex = Object.values(INSTRUMENTS)
+    const instrumentsByIndex = Object.values(INSTRUMENTS)
 
-  this.board = []
+    this.board = []
 
-  for (let i = 0; i < beatLength; i++) {
-    this.board.push(new Mix())
-    for (let j = 0; j < mappedMatrix.length; j++) {
-      if (mappedMatrix[j][i] !== '') {
-        this.board[i].add(`../assets/audio/${instrumentsByIndex[j]}1.wav`)
-      }
+    for (let i = 0; i < beatLength; i++) {
+        this.board.push(new Mix())
+        for (let j = 0; j < mappedMatrix.length; j++) {
+            if (mappedMatrix[j][i] !== '') {
+                this.board[i].add(`../assets/audio/${instrumentsByIndex[j]}1.wav`)
+            }
+        }
     }
-  }
 }
 
-SoundBoard.prototype.play = async function (delay) {
-  for (let i = 0; i < 4; i++) {
-    this.board[i].play()
-    await sleep(delay)
-  }
+SoundBoard.prototype.play = async function(delay) {
+    for (let i = 0; i < 4; i++) {
+        this.board[i].play()
+        await sleep(delay)
+    }
 }
 
 function Mix() {
-  this.channels = []
+    this.channels = []
 }
 
-Mix.prototype.add = function (audioSrc) {
-  this.channels.push(new Channel(audioSrc))
+Mix.prototype.add = function(audioSrc) {
+    this.channels.push(new Channel(audioSrc))
 }
 
-Mix.prototype.play = async function () {
-  for (const channel of this.channels) {
-    channel.play()
-  }
+Mix.prototype.play = async function() {
+    for (const channel of this.channels) {
+        channel.play()
+    }
 }
 
 function Channel(src) {
-  this.audio = new Audio(src)
+    this.audio = new Audio(src)
 }
 
-Channel.prototype.play = function () {
-  this.audio.play()
+Channel.prototype.play = function() {
+    this.audio.play()
 }
