d3e8942e8d1f6ca32b91b370f723243f0b7110bb
Dontae Esdaile <desdaile@udel.edu>
2021-12-09 09:51:29 -0500

    profile

--- js/profile.js
+++ js/profile.js
@@ -20,6 +20,7 @@ function initialize() {
         }
     });
 }
+let  userObj = {};
 
 function readUsersInfo() {
     $.ajax({
@@ -28,6 +29,21 @@ function readUsersInfo() {
         statusCode: {
             200: function (userInfo) {
                 console.log(userInfo) // Donte here is the object that has the user info
+                document.getElementById("user-name").innerHTML = userInfo.name;
+                document.getElementById("user-email").innerHTML = userInfo.email;
+                let bc = document.getElementById("beat-card");
+                if(userInfo.beats.length == 0)
+                {
+                    bc.style.display = "none";
+                    const para = document.createElement("h2");
+                    const node = document.createTextNode("You have no beats!");
+                    para.appendChildnode(node);
+                    const element = document.getElementById("your-beats");
+                    element.appendChild(para);
+                    console.log("You have no beats!");
+                }
+
+                //let userObj = userfInfo;
             },
             203: function (result) {
                 console.log(result)
